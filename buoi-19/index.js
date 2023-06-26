c9ca8b319acac0ef77e4a112cc7642e195d554df
ducdo <tvietduc122@gmail.com>
2023-06-26 18:27:50 +0700

    add function

--- buoi-19/index.js
+++ buoi-19/index.js
@@ -117,6 +117,144 @@ if (arrMin.length > 0) {
 }
 
 
+// chữa bài số nguyên tố
+// c1: Làm theo thông thường ai cũng hiểu
+// hàm kiểm tra số nguyên tố
+// quy định: hàm trả về true -> là số nguyên tố, false không phải nguyên tố
+// solution: tạo ra biến có 2 trạng thái true(số nguyên tố) false(không phải nguyên tố)
+function isPrime(number) {
+    let flagPrime = true;
+    // 1. nhỏ hơn 2
+    if(number < 2) {
+        flagPrime = false;
+    } else if(number > 2) {
+
+        for(let i = 2; i <= Math.sqrt(number); i++) {
+            console.log('i', i);
+            // nếu number chia hết cho bất kì số nào thi nó không phải nguyên tố
+            if(number % i === 0) {
+                flagPrime = false;
+                break;
+            }
+        }
+
+    }
+
+    return flagPrime;
+}
+
+// thực thi hàm
+const numberCheck = isPrime(49);
+if(numberCheck) {
+    console.log('đây là số nguyên tố');
+} else {
+    console.log('đây không phải là số nguyên tố');
+}
+
+// n < 2 -> không nguyên tố
+// n = 2 -> nguyên tố
+// n > 2 -> nó chia hết cho số nào trong
+// khoảng từ > 2 và nhỏ hơn n thì k phải nguyên tố
+
+// c2: level middle
+
+
+// c3: 
+
+// quản lí sản phẩm
+let produts = [
+    'Nokia',
+    'Samsung',
+    'Iphone',
+];
+
+// hàm show list danh sách sản phẩm
+function showList(produts) {
+    let listProduct = '';
+    for(let i = 0; i < produts.length; i++) {
+        listProduct = listProduct + `<li>${produts[i]}</li>`
+    }
+    listProduct =  `<ul>${listProduct}</ul>`;
+    document.write(listProduct);
+}
+
+// hàm thêm sản phẩm
+function addProduct(productName) {
+    produts.push(productName);
+    showList(produts);
+}
+// thêm sản phẩm
+addProduct('oppo');
+
+// hàm sửa sản phẩm
+// input: 
+// 1. vị trí cần sửa(index)
+// 2. giá trị cần sửa
+function editProduct(index, nameEdit) {
+    produts[index] = nameEdit;
+    showList(produts);
+}
+
+editProduct(0, 'Vertu');
+
+// hàm xóa sản phẩm
+// input: 
+// 1. vị trí cần sửa(index)
+function deleteProduct(index) {
+    // xóa sản phẩm
+    produts.splice(index, 1);
+    showList(produts);
+}
+deleteProduct(0);
+
+
+// tính tổng -- câu trả lời là: không
+function sumNumber(numbers) {
+    let sum = 0;
+    // biến chỉ truy cập trong hàm
+   
+    for(let i = 0; i < numbers.length; i++) {
+        sum = sum + numbers[i];
+    }
+    return sum;
+}
+
+console.log(sumNumber([1, 2, 3, 4, 5, 6]));
+
+// hàm conver chữ cái của mỗi từ đầu tiên thành hoa
+// let a = "hello WORD";
+// let b = "rIkkEi acaDEMY";
+// console.log(capitalize(a)); // Hello Word
+// consoloe.log(capitalize(v)); // Rikkei Academy
+
+// ->Hello Word
+function capitalize(textConverter) {
+    // 1. chuyển đổi thành chữ thường ->hello word
+    textConverter = textConverter.toLowerCase();
+    // 2. split ra thành mảng, các phần tử của mảng ngăn cách nhau ['hello', 'word']
+    let textConverterArr = textConverter.split(' ');
+    for(let i = 0; i < textConverterArr.length; i++) {
+
+        // 3. loop qua mảng và thay đổi kí tự đầu tiên của phần tử mảng đến in hoa (h -> H, w ->W)
+        let fistCharacterStr = textConverterArr[i][0];
+        let sectionFromIndex1Str = textConverterArr[i].slice(1);
+        let strConvertCapitalFirst = fistCharacterStr.toUpperCase() + sectionFromIndex1Str;
+
+        // 4. Các kí tự còn lại giữ nguyên ['Hello', 'Word']
+        textConverterArr[i] = strConvertCapitalFirst;
+    }
+
+    // 5. convert mảng về string -> join (' ') ->Hello Word
+    return textConverterArr.join(' ');
+}
+
+console.log(capitalize('rIkkEi acaDEMY'));
+
+
+
+
+
+
 
 
 
