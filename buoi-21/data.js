724e3cefee9ffac6f0a75491ed0b53461766d435
ducdo <tvietduc122@gmail.com>
2023-06-29 13:33:50 +0700

    add to do list

---
+++ buoi-21/data.js
@@ -0,0 +1,23 @@
+let todos = [
+
+    {id: 1, name: 'Quét nhà'},
+
+    {id: 2, name: 'Giặt quần áo'},
+
+    {id: 3, name: 'Nấu cớm'}
+
+
+];
+// biến ở phạm vi global (vì sao?)
+// vì nó không bị giới hạn trong dấu {}(block)
+
+
+
+// // cách lấy ra object
+// console.log(todos[0]);
+// // truy vấn lấy value object
+// console.log(todos[0].name);
+// console.log(todos[0].id);
+// // cách 2 lấy ra name
+// console.log(todos[0]['name']);
+
